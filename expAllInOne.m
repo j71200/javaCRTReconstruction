dc8b4e48191490c76fe025deaefa598d1fb8b302
j71200 <j71200@gmail.com>
2016-06-07 14:31:33 +0800

    exp2

--- expAllInOne.m
+++ expAllInOne.m
@@ -8,34 +8,35 @@ clc
 % 256
 % 512
 
-scaleRatio = 512/512;
+scaleRatio = 16/512;
 repeatTimes = 10;
 
 elapsedKeyGen = zeros(repeatTimes, 1);
-elapsedReconstruct = zeros(repeatTimes, 1);
+% elapsedReconstruct = zeros(repeatTimes, 1);
 elapsedEncryption = zeros(repeatTimes, 1);
 elapsedDecryption = zeros(repeatTimes, 1);
-elapsedInvRecon = zeros(repeatTimes, 1);
+% elapsedInvRecon = zeros(repeatTimes, 1);
 
 disp(['scaleRatio: ' num2str(scaleRatio)]);
 for idx = 1:repeatTimes
 	disp(['repeat: ' num2str(idx)]);
 	a = clock;
 	disp(['Start at ' num2str(a(2)) '/' num2str(a(3)) ' ' num2str(a(4)) ':' num2str(a(5))]);
-	[elapsedKeyGen(idx), elapsedReconstruct(idx), elapsedEncryption(idx), elapsedDecryption(idx), elapsedInvRecon(idx)] = expParaEncryption(scaleRatio);
+	% [elapsedKeyGen(idx), elapsedReconstruct(idx), elapsedEncryption(idx), elapsedDecryption(idx), elapsedInvRecon(idx)] = expParaEncryption(scaleRatio);
+	[elapsedKeyGen, elapsedEncryption, elapsedDecryption] = expParaEncryptionNoRecon(scaleRatio);
 end
 
 averageKeyGen = mean(elapsedKeyGen);
-averageReconstruct = mean(elapsedReconstruct);
+% averageReconstruct = mean(elapsedReconstruct);
 averageEncryption = mean(elapsedEncryption);
 averageDecryption = mean(elapsedDecryption);
-averageInvRecon = mean(elapsedInvRecon);
+% averageInvRecon = mean(elapsedInvRecon);
 
 elapsedKeyGen
-elapsedReconstruct
+% elapsedReconstruct
 elapsedEncryption
 elapsedDecryption
-elapsedInvRecon
+% elapsedInvRecon
 
 
 
---
+++ expParaEncryptionNoRecon.m
@@ -0,0 +1,142 @@
+function [elapsedKeyGen, elapsedEncryption, elapsedDecryption] = expParaEncryptionNoRecon(scaleRatio)
+% close all
+% clear
+% clc
+
+
+inputImage = imread('images/lena_gray.png');
+smallInputImage = imresize(inputImage, scaleRatio);
+
+% smallInputImage = imresize(inputImage, 0.03125); % smallInputImage is 16 x 16
+% smallInputImage = imresize(inputImage, 1/4); % smallInputImage is 128 x 128
+% smallInputImage = inputImage;
+
+% figure
+% imshow(smallInputImage)
+[height, width] = size(smallInputImage);
+totalPixels = height * width;
+
+% ========================
+% KeyGen
+% ========================
+% RSA-768 (232 digits)
+ticKeyGen = tic;
+p_bigd = java.math.BigDecimal('33478071698956898786044169848212690817704794983713768568912431388982883793878002287614711652531743087737814467999489');
+q_bigd = java.math.BigDecimal('36746043666799590428244633799627952632279158164343087642676032283815739666511279233373417143396810270092798736308917');
+
+[n_bigd, g_bigd, lambda_bigd, mu_bigd] = javaPaillierKeygen(p_bigd, q_bigd);
+elapsedKeyGen = toc(ticKeyGen);
+
+vPlaintext = reshape(smallInputImage, totalPixels, 1);
+
+
+% ========================
+% Reconstruction
+% ========================
+% load('mat/primeList_502_bigd.mat');
+
+% reconFactor = 208;
+% vModului_bigd = primeList_502_bigd(1:reconFactor);
+
+% ticReconstruct = tic;
+% cReconData_bigd = expParaCRTReconstruct(vPlaintext, vModului_bigd);
+% elapsedReconstruct = toc(ticReconstruct);
+
+cReconData_bigd = cell(totalPixels, 1);
+parfor idx = 1:totalPixels
+	cReconData_bigd{idx} = dbl2bigd(vPlaintext(idx));
+end
+
+% ========================
+% Encryption
+% ========================
+ticEncryption = tic;
+cCiphertext_bigd = expParaPaillierEncryption(cReconData_bigd, n_bigd, g_bigd);
+elapsedEncryption = toc(ticEncryption);
+
+% ========================
+% Decryption
+% ========================
+ticDecryption = tic;
+cDecryptedMessage_bigd = expParaPaillierDecryption(cCiphertext_bigd, n_bigd, lambda_bigd, mu_bigd);
+elapsedDecryption = toc(ticDecryption);
+
+% ========================
+% Inverse-reconstruction
+% ========================
+% cDecryptedMessage_bigd = cReconData_bigd;
+% ticInvRecon = tic;
+% vRecoveredMessage = expParaCRTInvReconstruct(cDecryptedMessage_bigd, vModului_bigd, totalPixels);
+% elapsedInvRecon = toc(ticInvRecon);
+
+
+% ========================
+% Examining
+% ========================
+% recoveredImage = uint8(vRecoveredMessage);
+% recoveredImage = reshape(recoveredImage, height, width);
+% % figure
+% % imshow(recoveredImage)
+% if nnz(double(recoveredImage) - double(smallInputImage)) ~= 0
+% 	warning('recoveredImage is not equals to smallInputImage on scaleRatio %f.', scaleRatio);
+% end
+
+
+% =========== Without the reconstruction ===========
+% General time
+% 16x16 pixel needs 218 s
+
+% Parallel time
+% 16x16 pixel needs 108 s
+
+% General time on Server
+% 16x16 pixel needs 334 s
+
+% Parallel time on Server
+% 16x16 pixel needs 37 s
+
+% =========== 16x16 ===========
+% General time
+% 16x16 pixel needs  s
+
+% Parallel time
+% 16x16 pixel needs 108 s
+% 16x16 pixel needs 0.92 + 3.56 = 4.48 s
+
+% General time on Server
+% 16x16 pixel needs  s
+
+% Parallel time on Server
+% 16x16 pixel needs 37 s
+% 16x16 pixel needs 0.74 + 4.69 = 5.43 s
+
+% =========== 128x128 ===========
+% General time
+% 128x128 pixel needs  s
+
+% Parallel time
+% 128x128 pixel needs  s
+% 128x128 pixel needs 9.51 + 163.79 = 173.30 s
+
+% General time on Server
+% 128x128 pixel needs  s
+
+% Parallel time on Server
+% 128x128 pixel needs  s
+% 128x128 pixel needs 3.36 + 40.79 = 44.15 s
+
+% =========== 512x512 ===========
+% General time
+% 512x512 pixel needs  s
+
+% Parallel time
+% 512x512 pixel needs 133.84 + __ s
+
+% General time on Server
+% 512x512 pixel needs  s
+
+% Parallel time on Server
+% 512x512 pixel needs  s
+% 512x512 pixel needs 38.64 + 610.92 = 649.56 s
+
+end
