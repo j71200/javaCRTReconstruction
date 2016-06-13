62860ed4f80495d46f0325d22de5ddac015ccc5d
j71200 <j71200@gmail.com>
2016-06-13 14:30:13 +0800

    exp3

--- expAllInOneExeTime.m
+++ expAllInOneExeTime.m
@@ -9,8 +9,8 @@ clc
 % 256
 % 512
 
-scaleRatio = 512/512;
-repeatTimes = 1;
+scaleRatio = 16/512;
+repeatTimes = 10;
 
 elapsedKeyGen = zeros(repeatTimes, 1);
 elapsedReconstruct = zeros(repeatTimes, 1);
@@ -19,13 +19,13 @@ elapsedDecryption = zeros(repeatTimes, 1);
 elapsedInvRecon = zeros(repeatTimes, 1);
 
 disp(['scaleRatio: ' num2str(scaleRatio)]);
-for repeatIdx = 1:repeatTimes
-	disp(['repeat: ' num2str(repeatIdx)]);
+for idx = 1:repeatTimes
+	disp(['repeat: ' num2str(idx)]);
 	a = clock;
 	disp(['Start at ' num2str(a(2)) '/' num2str(a(3)) ' ' num2str(a(4)) ':' num2str(a(5))]);
 
 
-
+	
 	inputImage = imread('images/lena_gray.png');
 	smallInputImage = imresize(inputImage, scaleRatio);
 	[height, width] = size(smallInputImage);
@@ -44,42 +44,6 @@ for repeatIdx = 1:repeatTimes
 
 	vPlaintext = reshape(smallInputImage, totalPixels, 1);
 
-
-	% ========================
-	% Genertate watermark
-	% ========================
-	load('mat/data_wm256_pt256x256.mat');
-
-	normHeight = 512;
-	normWidth = 512;
-
-	wmSize = 50; % 8KB
-	watermark = randi([0, 1], wmSize, 1);
-	patternSize = 256 * 256; % 必須大於大於 wmSize才行
-	patterns = sign(randn(patternSize, wmSize));
-
-	wmSignature1 = patterns * (2*watermark - 1);
-
-	[zigzagColMajor ~] = genaralZigzag(normHeight, normWidth);
-	wmSignature2 = zeros(normHeight, normWidth);
-	imageArea = length(zigzagColMajor);
-	middle_band_idx = zigzagColMajor(1+3*imageArea/8:3*imageArea/8 + patternSize);
-	wmSignature2(middle_band_idx) = wmSignature1;
-
-	wmSignature2_idct = idct2(wmSignature2);
-
-	% wmSignature2_idct_trick = round(wmSignature2_idct);
-	% trickShift = min(min(wmSignature2_idct_trick));
-	% wmSignature2_idct_trick_uint = wmSignature2_idct_trick - trickShift + 1;
-	% wmSignature2_idct_trick_uint = uint64(wmSignature2_idct_trick_uint);
-
-	% reconWmSignature2_idct_trick_bigd = javaCRTReconstruct(wmSignature2_idct_trick_uint, modulus_bigd);
-
-	trickShift = min(min(wmSignature2_idct));
-	wmSignature2_idct_trick = wmSignature2_idct - trickShift;
-	wmSignature2_idct_trick = round(wmSignature2_idct_trick) + 1;
-
-
 	% ========================
 	% Reconstruction image
 	% ========================
@@ -92,39 +56,20 @@ for repeatIdx = 1:repeatTimes
 	cReconData_bigd = expParaCRTReconstruct(vPlaintext, vModului_bigd);
 	elapsedReconstruct = toc(ticReconstruct);
 
-	% ========================
-	% Reconstruction watermark
-	% ========================
-	vWmSignature2_idct_trick = reshape(wmSignature2_idct_trick, normWidth*normHeight, 1);
-	cReconWatermark_bigd = expParaCRTReconstruct(vWmSignature2_idct_trick, vModului_bigd);
 
 	% ========================
-	% Encrypting image
+	% Encryption
 	% ========================
 	ticEncryption = tic;
 	cCiphertext_bigd = expParaPaillierEncryption(cReconData_bigd, n_bigd, g_bigd);
 	elapsedEncryption = toc(ticEncryption);
 
-	% ========================
-	% Encrypting watermark
-	% ========================
-	cEncryptedWatermark_bigd = expParaPaillierEncryption(cReconWatermark_bigd, n_bigd, g_bigd);
-
-
-	% ========================
-	% Embedding watermark
-	% ========================
-	cEncEmbImage_bigd = cell(length(cCiphertext_bigd), 1);
-	parfor idx = 1:length(cCiphertext_bigd)
-		cEncEmbImage_bigd{idx} = cCiphertext_bigd{idx} * cEncryptedWatermark_bigd{idx};
-		cEncEmbImage_bigd{idx} = cEncEmbImage_bigd{idx}.remainder(n_bigd.pow(2));
-	end
 
 	% ========================
 	% Decryption
 	% ========================
 	ticDecryption = tic;
-	cDecryptedMessage_bigd = expParaPaillierDecryption(cEncEmbImage_bigd, n_bigd, lambda_bigd, mu_bigd);
+	cDecryptedMessage_bigd = expParaPaillierDecryption(cCiphertext_bigd, n_bigd, lambda_bigd, mu_bigd);
 	elapsedDecryption = toc(ticDecryption);
 
 	% ========================
@@ -152,11 +97,9 @@ for repeatIdx = 1:repeatTimes
 	% ========================
 	% To be deleted
 	% ========================
-	% [elapsedKeyGen(repeatIdx), elapsedReconstruct(repeatIdx), elapsedEncryption(repeatIdx), elapsedDecryption(repeatIdx), elapsedInvRecon(repeatIdx)] = expParaEncryption(scaleRatio);
-	% [elapsedKeyGen(repeatIdx), elapsedEncryption(repeatIdx), elapsedDecryption(repeatIdx)] = expParaEncryptionNoRecon(scaleRatio);
+	% [elapsedKeyGen(idx), elapsedReconstruct(idx), elapsedEncryption(idx), elapsedDecryption(idx), elapsedInvRecon(idx)] = expParaEncryption(scaleRatio);
+	% [elapsedKeyGen(idx), elapsedEncryption(idx), elapsedDecryption(idx)] = expParaEncryptionNoRecon(scaleRatio);
 end
-
-save('mat/expImportant.mat');
 
 
 elapsedKeyGen
--- test4.m
+++ test4.m
@@ -1,29 +1,22 @@
 clear('all');
 clc
 
-% ZERO_BIGD = java.math.BigDecimal('0');
-
-% ==========
-% KeyGen
-% ==========
 tic
-% RSA-576 (174 digits)
-p_bigd = java.math.BigDecimal('398075086424064937397125500550386491199064362342526708406385189575946388957261768583317');
-q_bigd = java.math.BigDecimal('472772146107435302536223071973048224632914695302097116459852171130520711256363590397527');
-[n_bigd, g_bigd, lambda_bigd, mu_bigd] = javaPaillierKeygen(p_bigd, q_bigd);
+% summation = uint64(0);
+summation = 0;
+repeatTimes = 1000;
+for idx = 1:repeatTimes
+	for jdx = 1:repeatTimes
+		for kdx = 1:repeatTimes
+			summation = summation + idx;
+		end
+	end
+end
 toc
 
-% ==========
-% Encrypt
-% ==========
-tic
-r_bigd = java.math.BigDecimal('3');
-m_bigd = java.math.BigDecimal('678');
-encryptedImage_bigd(idx) = javaPaillierEncrypt(m_bigd, n_bigd, g_bigd, r_bigd);
-toc
-
-
-
+summation
+a=2^32;
+uint64(a)
 
 
 
